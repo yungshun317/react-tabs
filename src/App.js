058342513f0db24f72b57d6cd290552e67ecd752
yungshun317 <seraph317@gmail.com>
2024-03-28 15:12:06 +0800

    [FEAT] Inspect state update batching

--- src/App.js
+++ src/App.js
@@ -97,7 +97,33 @@ function TabContent({ item }) {
   const [likes, setLikes] = useState(0);
 
   function handleInc() {
-    setLikes(likes + 1);
+      // setLikes(likes + 1);
+      setLikes((likes) => likes + 1);
+  }
+
+  function handleTripleInc() {
+      // Only once
+      // setLikes(likes + 1);
+      // setLikes(likes + 1);
+      // setLikes(likes + 1);
+
+      // Still once if `function handleInc() { setLike(likes + 1); }`
+      // handleInc();
+      // handleInc();
+      // handleInc();
+
+      setLikes((likes) => likes + 1);
+      setLikes((likes) => likes + 1);
+      setLikes((likes) => likes + 1);
+  }
+
+  function handleUndo() {
+      setShowDetails(true);
+      setLikes(0);
+  }
+
+  function handleUndoLater() {
+      setTimeout(handleUndo, 2000);
   }
 
   return (
@@ -113,13 +139,13 @@ function TabContent({ item }) {
           <div className="hearts-counter">
             <span>{likes} ❤️</span>
             <button onClick={handleInc}>+</button>
-            <button>+++</button>
+            <button onClick={handleTripleInc}>+++</button>
           </div>
         </div>
 
         <div className="tab-undo">
-          <button>Undo</button>
-          <button>Undo in 2s</button>
+          <button onClick={handleUndo}>Undo</button>
+          <button onClick={handleUndoLater}>Undo in 2s</button>
         </div>
       </div>
   );
--- src/index.js
+++ src/index.js
@@ -3,9 +3,20 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 
+// React 18
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
 );
+
+// React 17
+/*
+ReactDOM.render(
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>
+    document.getElementById("root")
+);
+*/
